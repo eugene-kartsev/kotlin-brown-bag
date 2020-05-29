4790c35f725226d068b1b69cf88280dd763a2bbf
Eugene Kartsev <ykartsev@amazon.com>
2020-05-28 17:42:49 -0700

    Changes NullableTypes and Coroutines examples

--- src/_05NullableTypes.kt
+++ src/_05NullableTypes.kt
@@ -1,15 +1,29 @@
 fun main() {
-    sayHi("Eugene")
+    val str1: String = "Eugene"
+    val str2 = "Eugene"
+    // The following line will not compile,
+    // because "str2" is effectively final
+    // and cannot be re-assigned
+    //
+    // str2 = "Someone else"
 
+    var str3 = "Eugene"
+    str3 = "Someone else"
+
+    sayHi(str3)
+
+    val str4: String? = null
     // The following line is not going to compile.
     // It will work only if we change the definition
     // of sayHi function to:
     //     fun sayHi(str: String?) {}
-    // sayHi(null)
+    // sayHi(str4)
 }
 
-
 fun sayHi(str: String) {
+    // Cannot re-assign input parameters
+    // str = ""
+
     println("Hey, $str")
     // println("Hey, ${str ?: "noname"}")
 }
--- src/_16Coroutines.kt
+++ src/_16Coroutines.kt
@@ -1,19 +1,25 @@
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.Dispatchers.IO
+import kotlin.system.measureTimeMillis
 
-fun main() = runBlocking {
+fun main() = runBlocking(IO) {
     // try with 1000_000L !!
-    for (i in 1..10_000L) {
+    for (i in 1..2L) {
         launch {
-            square(i)
+            println("$i ^ 2 = ${square(i)}")
         }
-        println("i = $i")
     }
+
+    println("done")
 }
 
-suspend fun square(num: Long) {
-    delay(10)
-    println("$num ^ 2 = ${num * num}")
+suspend fun square(num: Long): Long {
+    println("$num: Thread name: ${Thread.currentThread().name}")
+    delay(timeMillis = (10..100L).random())
+    println("$num: Thread name: ${Thread.currentThread().name}")
+
+    return num * num
 }
 
